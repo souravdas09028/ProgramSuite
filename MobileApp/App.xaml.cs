2e59c49ebd87bb863116e410e9736f5720ea6480
sourav <souravdas09028@gmail.com>
2025-08-24 03:17:34 +0600

    Add project files.

---
+++ MobileApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MobileApp;
+
+public partial class App : Microsoft.Maui.Controls.Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "MobileApp" };
+	}
+}
